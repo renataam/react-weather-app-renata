2e588d30a24c711c1552a6fb8838bd7d923ddaf0
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-12 14:25:49 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Wether App</h1>
-      <footer>
-        This project was code by Renata Monteiro ad is{" "}
-        <a
-          href="https://github.com/renataam/react-weather-app-renata"
-          target="blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was code by Renata Monteiro ad is{" "}
+          <a
+            href="https://github.com/renataam/react-weather-app-renata"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

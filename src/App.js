2b4b056c81d3a67d28b1b7965200a79a037ee640
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-12 11:03:42 -0300

    start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     <h1>Wether App</h1>
+     <footer>
+     <a
+     href=""
+     target="blank"
+     >
+      open-sourced on GitHub</a> 
+     </footer>
+    </div>
+  );
+}
+

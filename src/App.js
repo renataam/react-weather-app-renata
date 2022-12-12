a35ec11c67443dcf316cf06f5f2c5aa4c3670a4c
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-12 14:07:40 -0300

    footer link

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     <h1>Wether App</h1>
-     <footer>
-     <a
-     href=""
-     target="blank"
-     >
-      open-sourced on GitHub</a> 
-     </footer>
+      <h1>Wether App</h1>
+      <footer>
+        This project was code by Renata Monteiro ad is{" "}
+        <a
+          href="https://github.com/renataam/react-weather-app-renata"
+          target="blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }

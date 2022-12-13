558210e0a1d29ec015cfd496a2972bbd50fd8413
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-13 14:29:53 -0300

    added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,66 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios"; 
 import "./Weather.css";
 
 
-export default function Weather() {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready:false });
     function handleResponse(response){
         console.log(response.data);
+        setWeatherData({
+          ready: true,
+          temperature: response.data.main.temp,
+          humidity: response.data.main.humidity,
+          date: "terça",
+          description: response.data.weather[0].description,
+          iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+          wind: response.data.wind.speed,
+          city: response.data.name,
+        });
     }
 
-    const apiKey = "170ed67c56f7d3751961a6f26123ed61";
-    let city = "London"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <h1>Rio de Janeiro</h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="Mostly Cloudy"
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">29</span>
-                <span className="unit">ºC</span>
+    if (weatherData.ready) {
+      return (
+        <div className="Weather">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city"
+                  className="form-control"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-primary w-100"
+                />
               </div>
             </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Preciptation: 15%</li>
-              <li>Humidity: 72%</li>
-              <li>Wind: 13km/h</li>
-            </ul>
+          </form>
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="clearfix">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <span className="temperature">
+                    {Math.round(weatherData.temperature)}
+                  </span>
+                  <span className="unit">ºC</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      const apiKey = "170ed67c56f7d3751961a6f26123ed61";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+
+      return "Loading...";
+    }
 }

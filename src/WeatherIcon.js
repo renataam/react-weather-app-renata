bb6b82924d949d9a5bb3d173fbe74b4a8db2d5a2
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-15 15:04:21 -0300

    added css forecast style

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
         color="#e9c46a"
-        size={52}
+        size={props.size}
         animate={true}
       />
     );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
             <div className="clearfix d-flex">
               <div className="float-left">
                 <WeatherIcon
-                  code={props.data.icon} />
+                  code={props.data.icon} size={52}/>
               </div>
               <div className="float-left">
                 <WeatherTemperature celsius={props.data.temperature} />

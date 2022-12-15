6a6d3c944dacbead20ed152908acfcd379fe6c3e
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-15 15:54:34 -0300

    api forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+    function handleResponse(response){
+        console.log(response.data);
+    }
+
+
+    let apiKey = "170ed67c56f7d3751961a6f26123ed61";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse); 
+
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -20,3 +33,4 @@ export default function WeatherForecast() {
       </div>
     );
 }
+

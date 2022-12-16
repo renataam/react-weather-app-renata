fff9fd53bce7b56540014bef0d8a40c1282b19b5
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-15 21:15:38 -0300

    fixed things

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,15 @@ export default function WeatherForecast(props) {
         setLoaded(true);
     }
 
+    function load(){
+        let apiKey = "53f3bc1f5d348c44be3e3754c7185573";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+    }
+
     if (loaded) {
         return (
           <div className="WeatherForecast">
@@ -35,12 +44,7 @@ export default function WeatherForecast(props) {
           </div>
         );
     } else {
-        let apiKey = "53f3bc1f5d348c44be3e3754c7185573";
-        let longitude = props.coordinates.lon;
-        let latitude = props.coordinates.lat;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-        axios.get(apiUrl).then(handleResponse);
+        load();
 
         return null;
     }
